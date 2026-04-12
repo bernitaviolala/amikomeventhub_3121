6eab9ae33f462e6590ce6ee0f8190c5005ed0b2e
bernitaviolala <bernita422@students.amikom.ac.id>
2026-04-12 22:22:43 +0700

    tugas 1

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -25,4 +23,5 @@ Route::get('/katalog', function () {
 Route::get('/bantuan', function () {
     return view('bantuan');
 });
+
 
