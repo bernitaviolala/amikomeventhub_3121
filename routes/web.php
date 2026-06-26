dcfc9bc4d31e4712fb6cbe130fa027fa0ca6385c
bernitaviolala <bernita422@students.amikom.ac.id>
2026-06-26 08:17:26 +0700

    praktikum11

--- routes/web.php
+++ routes/web.php
@@ -69,4 +69,9 @@ Route::prefix('admin')->name('admin.')->group(function () {
         Route::get('transactions', [\App\Http\Controllers\Admin\TransactionController::class, 'index'])->name('transactions.index');
 
     });
+
 });
+
+Route::get('/payment/{order_id}',[\App\Http\Controllers\CheckoutController::class,'payment'])->name('checkout.payment');
+
+Route::get('/success/{order_id}', [\App\Http\Controllers\CheckoutController::class, 'success'])->name('checkout.success');
